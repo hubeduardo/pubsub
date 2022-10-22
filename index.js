e67f9bf4f9a63d1e136be477c8fb08ead944b999
hubeduardo <eduardof.paulo@gmail.com>
2022-10-22 17:42:56 -0300

    ajustando cenario

--- index.js
+++ index.js
@@ -1,38 +1,46 @@
-const WebSocketServer = require('ws').Server;
 const express = require('express');
-const path = require('path');
-const server = require('http').createServer();
-const PubSubManager = require('./pubsub');
+const { WebPubSubServiceClient } = require('@azure/web-pubsub');
+const { WebPubSubEventHandler } = require('@azure/web-pubsub-express');
 
 const app = express();
-const pubSubManager = new PubSubManager();
+const hubName = 'Sample_ChatApp';
+const port = 8080;
 
-app.use(express.static(path.join(__dirname, '/public')));
-const wss = new WebSocketServer({ server: server });
-wss.on('connection', (ws, req) => {
-    console.log(`Connection request from: ${req.connection.remoteAddress}`);
-    ws.on('message', (data) => {
-        console.log('data: ' + data);
-        const json = JSON.parse(data);
-        const request = json.request;
-        const message = json.message;
-        const channel = json.channel;
-
-        switch (request) {
-            case 'PUBLISH':
-                pubSubManager.publish(ws, channel, message);
-                break;
-            case 'SUBSCRIBE':
-                pubSubManager.subscribe(ws, channel);
-                break;
-        }
+let connectionString = process.env.WebPubSubConnectionString;
+console.log(connectionString)
+let serviceClient = new WebPubSubServiceClient(connectionString, hubName);
+let handler = new WebPubSubEventHandler(hubName, {
+  path: '/eventhandler',
+  onConnected: async req => {
+    console.log(`${req.context.userId} connected`);
+    await serviceClient.sendToAll({
+      type: "system",
+      message: `${req.context.userId} joined`
     });
-    ws.on('close', () => {
-        console.log('Stopping client connection.');
-    });
+  },
+  handleUserEvent: async (req, res) => {
+    if (req.context.eventName === 'message') {
+      await serviceClient.sendToAll({
+        from: req.context.userId,
+        message: req.data
+      });
+    }
+    res.success();
+  }
 });
 
-server.on('request', app);
-server.listen(8080, () => {
-    console.log('Server listening on http://localhost:8080');
+app.use(handler.getMiddleware());
+app.get('/negotiate', async (req, res) => {
+  let id = req.query.id;
+  if (!id) {
+    res.status(400).send('missing user id');
+    return;
+  }
+  let token = await serviceClient.getClientAccessToken({ userId: id });
+  res.json({
+    url: token.url
+  });
 });
+
+app.use(express.static('public'));
+app.listen(port, () => console.log(`Event handler listening at http://localhost:${port}${handler.path}`));
